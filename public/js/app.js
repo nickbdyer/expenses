234c76dea0aee1448a873520635b8d7d61d305b5
Nick Dyer <nbdyer@gmail.com>
2015-02-17 17:51:30 +0000

    Posts form expense item onto page

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
 $(document).on('ready', function() {
   $('form').on('submit', function(event) {
     event.preventDefault();
-    $.post('/', $('form').serialize(), function(val) {
-      console.log(val)
+    $.post('/', $('form').serialize(), function(data) {
+      var data = JSON.parse(data);
+      $('#expense_list').prepend('<li class="expense_item">£' + data["amount"] +
+          ' - ' + data["category"] + '</li>');
     });
   });
 });
