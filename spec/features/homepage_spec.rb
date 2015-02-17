bfbde496d9f02747754a68de444b96ec27fbf3db
Andrew Snead <andrewcsnead@gmail.com>
2015-02-17 16:23:30 +0000

    added homepage form test

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 feature 'Homepage' do
 
-  scenario 'the page has a title' do
+  scenario 'has a title' do
     visit '/'
     expect(page).to have_title "Make Money Money!"
+  end
+
+  scenario 'has a form to submit an expense' do
+    visit '/'
+    expect(page).to have_selector('form')
   end
   
 end
